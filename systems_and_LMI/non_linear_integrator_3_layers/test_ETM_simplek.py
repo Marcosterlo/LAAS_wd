0693729634269a9663a0061ffa3602af2dc4eae3
Marco Sterlini <marcosterlo@gmail.com>
2024-10-29 11:25:45 +0100

    Refactor NonLinearPendulum_NN_kETM.py and test_ETM_simplek.py

---
+++ systems_and_LMI/non_linear_integrator_3_layers/test_ETM_simplek.py
@@ -0,0 +1,156 @@
+from systems_and_LMI.systems.NonLinearPendulum_NN_kETM import NonLinPendulum_NN_kETM
+from systems_and_LMI.systems.NonLinearPendulum_NN_ETM import NonLinPendulum_NN_ETM
+import numpy as np
+import matplotlib.pyplot as plt
+
+ref = 0.0
+s = NonLinPendulum_NN_kETM(ref)
+s1 = NonLinPendulum_NN_ETM(ref)
+nsteps = 300
+P = np.load('P.npy')
+
+states = []
+inputs = []
+events = []
+etas = []
+lyap = []
+
+states1 = []
+inputs1 = []
+events1 = []
+etas1 = []
+lyap1 = []
+
+in_ellip = False
+while not in_ellip:
+  theta0 = np.random.uniform(-np.pi/2, np.pi/2)
+  vtheta0 = np.random.uniform(-s.max_speed, s.max_speed)
+  ref = np.random.uniform(-0.1, 0.1)
+  ref = 0.0
+  s = NonLinPendulum_NN_kETM(ref)
+  s1 = NonLinPendulum_NN_ETM(ref)
+  x0 = np.array([[theta0], [vtheta0], [0.0]])
+  if (x0 - s.xstar).T @ P @ (x0 - s.xstar) <= 1 and (x0 - s1.xstar).T @ P @ (x0 - s1.xstar) <= 1:
+    in_ellip = True
+    s.state = x0
+    s1.state = x0
+
+print(f"Initial state: theta_0: {theta0:.2f}, vtheta_0: {vtheta0:.2f}, constant disturbance: {ref:.2f}")
+
+for i in range(nsteps):
+  state, u, e, eta = s.step() 
+  state1, u1, e1, eta1 = s1.step()
+  states.append(state)
+  inputs.append(u)
+  events.append(e)
+  lyap.append((state - s.xstar).T @ P @ (state - s.xstar) + 2*eta[0] + 2*eta[1] + 2*eta[2])
+
+  etas.append(eta)
+  states1.append(state1)
+  inputs1.append(u1)
+  events1.append(e1)
+  lyap1.append((state1 - s1.xstar).T @ P @ (state1 - s1.xstar) + 2*eta1[0] + 2*eta1[1] + 2*eta1[2])
+  etas1.append(eta1)
+
+states = np.array(states)
+inputs = np.array(inputs)
+etas = np.array(etas)
+events = np.array(events)
+lyap = np.array(lyap)
+
+states1 = np.array(states1)
+inputs1 = np.array(inputs1)
+etas1 = np.array(etas1)
+events1 = np.array(events1)
+lyap1 = np.array(lyap1)
+
+layer1_trigger_1 = np.sum(events[:, 0]) / nsteps * 100
+layer2_trigger_1 = np.sum(events[:, 1]) / nsteps * 100
+layer3_trigger_1 = np.sum(events[:, 2]) / nsteps * 100
+
+print(f"New ETM system:\n")
+print(f"Layer 1 has been triggered {layer1_trigger_1:.1f}% of times")
+print(f"Layer 2 has been triggered {layer2_trigger_1:.1f}% of times")
+print(f"Layer 3 has been triggered {layer3_trigger_1:.1f}% of times")
+
+layer1_trigger_2 = np.sum(events1[:, 0]) / nsteps * 100
+layer2_trigger_2 = np.sum(events1[:, 1]) / nsteps * 100
+layer3_trigger_2 = np.sum(events1[:, 2]) / nsteps * 100
+
+print(f"\nOld ETM system:\n")
+print(f"Layer 1 has been triggered {layer1_trigger_2:.1f}% of times")
+print(f"Layer 2 has been triggered {layer2_trigger_2:.1f}% of times")
+print(f"Layer 3 has been triggered {layer3_trigger_2:.1f}% of times")
+
+for i, event in enumerate(events):
+  if not event[0]:
+    events[i][0] = None
+  if not event[1]:
+    events[i][1] = None
+  if not event[2]:
+    events[i][2] = None
+
+for i, event in enumerate(events1):
+  if not event[0]:
+    events1[i][0] = None
+  if not event[1]:
+    events1[i][1] = None
+  if not event[2]:
+    events1[i][2] = None
+
+timegrid = np.arange(0, nsteps)
+
+fig, axs = plt.subplots(4, 1)
+axs[0].plot(timegrid, np.squeeze(inputs), label='Control input (new ETM)')
+axs[0].plot(timegrid, np.squeeze(inputs1), label='Control input (old ETM)')
+axs[0].plot(timegrid, np.squeeze(inputs)*events[:,2], marker='o', markerfacecolor='none')
+axs[0].plot(timegrid, np.squeeze(inputs1)*events1[:,2], marker='o', markerfacecolor='none')
+axs[0].set_xlabel('Time steps')
+axs[0].set_ylabel('Values')
+axs[0].legend()
+axs[0].grid(True)
+
+axs[1].plot(timegrid, np.squeeze(states[:, 0] - s.xstar[0]), label='Position new ETM')
+axs[1].plot(timegrid, np.squeeze(states[:, 0] - s.xstar[0])*events[:,2], marker='o', markerfacecolor='none')
+axs[1].plot(timegrid, np.squeeze(states1[:, 0] - s1.xstar[0]), label='Position old ETM')
+axs[1].plot(timegrid, np.squeeze(states1[:, 0] - s1.xstar[0])*events1[:,2], marker='o', markerfacecolor='none')
+axs[1].set_xlabel('Time steps')
+axs[1].set_ylabel('Values')
+axs[1].legend()
+axs[1].grid(True)
+
+axs[2].plot(timegrid, np.squeeze(states[:, 1] - s.xstar[1]), label='Velocity new ETM')
+axs[2].plot(timegrid, np.squeeze(states[:, 1] - s.xstar[1])*events[:,2], marker='o', markerfacecolor='none')
+axs[2].plot(timegrid, np.squeeze(states1[:, 1] - s1.xstar[1]), label='Velocity old ETM')
+axs[2].plot(timegrid, np.squeeze(states1[:, 1] - s1.xstar[1])*events1[:,2], marker='o', markerfacecolor='none')
+axs[2].set_xlabel('Time steps')
+axs[2].set_ylabel('Values')
+axs[2].legend()
+axs[2].grid(True)
+
+axs[3].plot(timegrid, np.squeeze(states[:, 2] - s.xstar[2]), label='Integrator state')
+axs[3].plot(timegrid, np.squeeze(states[:, 2] - s.xstar[2])*events[:,2], marker='o', markerfacecolor='none')
+axs[3].plot(timegrid, np.squeeze(states1[:, 2] - s1.xstar[2]), label='Integrator state new ETM')
+axs[3].plot(timegrid, np.squeeze(states1[:, 2] - s1.xstar[2])*events1[:,2], marker='o', markerfacecolor='none')
+axs[3].set_xlabel('Time steps')
+axs[3].set_ylabel('Values')
+axs[3].legend()
+axs[3].grid(True)
+
+plt.show()
+
+plt.plot(timegrid, np.squeeze(etas[:, 0]), label='Eta 1')
+plt.plot(timegrid, np.squeeze(etas[:, 1]), label='Eta 2')
+plt.plot(timegrid, np.squeeze(etas[:, 2]), label='Eta 3')
+plt.plot(timegrid, np.squeeze(etas1[:, 0]), label='Eta 1 old')
+plt.plot(timegrid, np.squeeze(etas1[:, 1]), label='Eta 2 old')
+plt.plot(timegrid, np.squeeze(etas1[:, 2]), label='Eta 3 old')
+plt.legend()
+plt.grid(True)
+plt.show()
+
+plt.plot(timegrid, np.squeeze(lyap), label='Lyapunov function')
+plt.plot(timegrid, np.squeeze(lyap1), label='Lyapunov function old etm')
+plt.legend()
+plt.grid(True)
+plt.show()
---
+++ systems_and_LMI/systems/NonLinearPendulum_NN_kETM.py
@@ -0,0 +1,67 @@
+from systems_and_LMI.systems.NonLinearPendulum_NN_ETM import NonLinPendulum_NN_ETM
+import systems_and_LMI.systems.nonlin_dynamic_ETM.params as params
+import os
+import numpy as np
+import torch.nn as nn
+import torch
+
+class NonLinPendulum_NN_kETM(NonLinPendulum_NN_ETM):
+  
+  def __init__(self, reference=0.0):
+    super().__init__(reference)
+
+    Omega_name = os.path.abspath(__file__ + '/../nonlin_dynamic_ETM/Omega.npy')
+    Omega = np.load(Omega_name)
+    Ck = np.zeros((self.nphi, self.nx))
+    Ck[:, 0] = 1e5
+
+    self.Omega = []
+    self.Ck = []
+    for i in range(self.nlayers - 1):
+      self.Omega.append(Omega[i*self.neurons[i]:(i+1)*self.neurons[i], i*self.neurons[i]:(i+1)*self.neurons[i]])
+      self.Ck.append(Ck[i*self.neurons[i]:(i+1)*self.neurons[i], :])
+  
+  def forward(self):
+    func = nn.Hardtanh()
+    e = np.zeros(self.nlayer - 1)
+    x = self.state.reshape(1, self.W[0].shape[1]) 
+    val = np.zeros(self.nlayer - 1)
+
+    for l in range(self.nlayer - 1):
+      if l == 0:
+        nu = self.layers[l](torch.tensor(x)).detach().numpy().reshape(self.W[l].shape[0], 1)
+      else:
+        nu = self.layers[l](torch.tensor(omega.reshape(1, self.W[l].shape[1]))).detach().numpy().reshape(self.W[l].shape[0], 1)
+      
+      vec1 = (nu - self.last_w[l]).T
+      T = self.T[l]
+      vec2 = (self.G[l] @ (self.state - self.xstar) - (self.last_w[l] - self.wstar[l]))
+      vecr = (self.state - self.xstar).T @ self.Ck[l].T @ self.Omega[l] @ self.Ck[l] @ (self.state - self.xstar)
+
+      check = (vec1 @ T @ vec2 - vecr > self.rho[l] * self.eta[l])[0][0]
+      
+      if check:
+        omega = func(torch.tensor(nu)).detach().numpy()
+        self.last_w[l] = nu
+        e[l] = 1
+        vec1 = (nu - omega).T
+        vec2 = (self.G[l] @ (self.state - self.xstar) - (omega - self.wstar[l]))
+        val[l] = (vec1 @ T @ vec2 - vecr)
+      
+      else:
+        val[l] = (vec1 @ T @ vec2 - vecr)
+        omega = self.last_w[l]
+        
+    l = self.nlayer - 1
+    nu = self.layers[l](torch.tensor(omega.reshape(1, self.W[l].shape[1]))).detach().numpy().reshape(self.W[l].shape[0], 1)[0][0]
+
+    for i in range(self.nlayer - 1):
+      self.eta[i] = (self.rho[i] + self.lam[i]) * self.eta[i] - val[i]
+    
+    return nu, e
+  
+if __name__ == "__main__":
+  
+  s = NonLinPendulum_NN_ETM(0.3)
+  x0 = np.array([[np.pi/3], [1.0], [0.0]])
+  s.state = x0
