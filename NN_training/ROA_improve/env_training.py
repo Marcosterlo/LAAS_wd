cb5fd0f905c708a7a1d5646c5e05da922f89ca2e
Marco Sterlini <marcosterlo@gmail.com>
2024-11-05 16:00:44 +0100

    Almeno ho caricato i pesi

--- NN_training/ROA_improve/env_training.py
+++ NN_training/ROA_improve/env_training.py
@@ -1,25 +1,88 @@
-import torch.nn as nn
 from stable_baselines3 import PPO
-from stable_baselines3.common.policies import ActorCriticPolicy
 from NN_training.environments.simple_pendulum_env import Simple_pendulum_env
-import numpy as np
-from NN_training.models.NeuralNet_simple import NeuralNet
+from stable_baselines3.common.callbacks import BaseCallback
 import torch
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 env = Simple_pendulum_env()
 
-deepl_model = NeuralNet()
+policy_kwargs = dict(activation_fn=torch.nn.Hardtanh,
+                     net_arch=[32, 32, 32])
 
-state_dict = torch.load('model.pth', map_location=torch.device('cpu'))
+model_rl = PPO("MlpPolicy", env, policy_kwargs=policy_kwargs, verbose=1)
 
-deepl_model.load_state_dict(state_dict)
-weights = {name: param.data.numpy() for name, param in deepl_model.named_parameters()}
+class CustomCallback(BaseCallback):
+  def __init__(self, verbose=0):
+    super().__init__(verbose)
+    self.param_path = 'model.pth'
 
-class CustomPolicy(ActorCriticPolicy):
-  def __init__(self, *args, **kwargs):
-    super(CustomPolicy, self).__init__(*args, **kwargs)
+  def _on_training_start(self):
+    state_dict = torch.load(self.param_path, map_location=torch.device(device))
+
+    # Layer 1
+    self.model.policy.mlp_extractor.policy_net[0].weight = nn.Parameter(state_dict['l1.weight'])
+    self.model.policy.mlp_extractor.policy_net[0].bias = nn.Parameter(state_dict['l1.bias'])
+
+    # Layer 2
+    self.model.policy.mlp_extractor.policy_net[2].weight = nn.Parameter(state_dict['l2.weight'])
+    self.model.policy.mlp_extractor.policy_net[2].bias = nn.Parameter(state_dict['l2.bias'])
     
-    for name, param in self.named_parameters():
-      param.data = torch.tensor(weights[name])
+    # Layer 3
+    self.model.policy.mlp_extractor.policy_net[4].weight = nn.Parameter(state_dict['l3.weight'])
+    self.model.policy.mlp_extractor.policy_net[4].bias = nn.Parameter(state_dict['l3.bias'])
+    
+    # Output layer
+    self.model.policy.action_net.weight = nn.Parameter(state_dict['l4.weight'])
+    self.model.policy.action_net.bias = nn.Parameter(state_dict['l4.bias'])
 
-model = PPO(CustomPolicy, env, verbose=1)
+    states = []
+    inputs = []
+
+    vec_env = self.model.get_env()
+    obs = vec_env.reset()
+    for i in range(1000):
+      action, _states = self.model.predict(obs, deterministic=True)
+      obs, rewards, done, info = vec_env.step(action)
+      states.append(obs)
+      inputs.append(action)
+
+    states = np.squeeze(np.array(states))
+    inputs = np.squeeze(np.array(inputs))
+
+    plt.plot(states[:,0], states[:,1])
+    plt.show()
+
+    plt.plot(inputs)
+    plt.show()
+
+  def _on_step(self):
+    return True
+
+
+callback = CustomCallback()
+
+model_rl.learn(total_timesteps=30000, callback=callback, progress_bar=True)
+
+states = []
+inputs = []
+
+vec_env = model_rl.get_env()
+obs = vec_env.reset()
+for i in range(1000):
+  action, _states = model_rl.predict(obs, deterministic=True)
+  obs, rewards, done, info = vec_env.step(action)
+  states.append(obs)
+  inputs.append(action)
+
+states = np.squeeze(np.array(states))
+inputs = np.squeeze(np.array(inputs))
+
+plt.plot(states[:,0], states[:,1])
+plt.show()
+
+plt.plot(inputs)
+plt.show()
--- NN_training/environments/simple_pendulum_env.py
+++ NN_training/environments/simple_pendulum_env.py
@@ -2,7 +2,6 @@ import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
 from stable_baselines3.common.env_checker import check_env
-import torch
 from systems_and_LMI.systems.NonLinPendulum_no_int import NonLinPendulum_no_int
 
 class Simple_pendulum_env(gym.Env):
@@ -10,17 +9,76 @@ class Simple_pendulum_env(gym.Env):
   def __init__(self):
     super(Simple_pendulum_env, self).__init__()
 
+    # Initialize the system to get the parameters
     self.system = NonLinPendulum_no_int()
+    self.max_speed = self.system.max_speed
+    self.max_torque = self.system.max_torque
+    self.dt = self.system.dt
+    self.g = self.system.g
+    self.m = self.system.m
+    self.l = self.system.l
 
+    # Action space definition
     self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)
 
-    self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(2,), dtype=np.float32)
+    # Observation space definition
+    self.lim_state = np.array([np.pi/2, self.max_speed], dtype=np.float32)
+    self.observation_space = spaces.Box(low=-self.lim_state, high=self.lim_state, shape=(2,), dtype=np.float32)
 
+    # Time variable
     self.time = 0
-    self.state = None
   
   def step(self, action):
+
+    # Theta and theta dot extraction
+    th, thdot = self.state 
     
-    action = np.clip(action, -1.0, 1.0)*self.system.max_torque
-    
-    return self.state, reward, done, {}
+    # Action clipping and de-normalization
+    action = np.clip(action, -1.0, 1.0)[0] * self.max_torque
+    # Theta normalization
+    th = (th + np.pi) % (2*np.pi) - np.pi
+
+    # Cost computation
+    cost = th**2 + 0.1*thdot**2 + 0.001*(action**2) - 1
+
+    # Dynamics update
+    new_state = self.system.A @ self.state.reshape(2,) + (self.system.B * action).reshape(2,) + (self.system.C * (np.sin(th) - th)).reshape(2,)
+
+    # State update
+    self.state = new_state.astype(np.float32)
+
+    truncated = False
+    terminated = False
+
+    # End of episode defintion
+    if self.time >= 200 - 1:
+      truncated = True
+
+    # Early termination definition    
+    if self.state not in self.observation_space:
+      terminated = True
+
+    # Time update
+    self.time += 1
+
+    return self.get_obs(), -float(cost), terminated, truncated, {}
+
+  def reset(self, seed=None):
+    super().reset(seed=seed)
+    # Random initial state
+    self.state = np.random.uniform(low=-self.lim_state, high=self.lim_state)
+    # Reset time
+    self.time = 0
+    return self.get_obs(), {}
+  
+  def get_obs(self):
+    th, thdot = self.state
+    return np.array([th, thdot], dtype=np.float32)
+  
+  def render(self):
+    th, thdot = self.get_obs()
+    print(f"Theta: {th:.2f}, Theta dot: {thdot:.2f}")
+  
+if __name__ == "__main__":
+  env = Simple_pendulum_env()
+  check_env(env)
