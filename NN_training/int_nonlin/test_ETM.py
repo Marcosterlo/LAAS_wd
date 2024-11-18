Fix on xstar computation

NN_training/int_nonlin/test_ETM.py
@@ -43,14 +43,14 @@ while not in_ellip:
   theta = np.random.uniform(-np.pi/2, np.pi/2)
   vtheta = np.random.uniform(-s.max_speed, s.max_speed)
   x0 = np.array([[theta], [vtheta], [0.0]])
+  ref = np.random.uniform(-ref_bound, ref_bound)
+  s = NonLinPendulum_kETM_train(W, b, ref)
   if (x0).T @ P @ (x0) <= 1.0:
     in_ellip = True
-    ref = np.random.uniform(-ref_bound, ref_bound)
     print(f"Initial state: theta0 = {theta*180/np.pi:.2f} deg, vtheta0 = {vtheta:.2f} rad/s, constant reference = {ref*180/np.pi:.2f} deg")
-    s = NonLinPendulum_kETM_train(W, b, ref)
     s.state = x0
-  
-nsteps = 300
+
+nsteps = 1000
 
 states = []
 inputs = []
@@ -101,14 +101,15 @@ for i, event in enumerate(events):
 fig, axs = plt.subplots(4, 1)
 axs[0].plot(timegrid, inputs, label='Control input')
 axs[0].plot(timegrid, inputs * events[:, 2], marker='o', markerfacecolor='none', linestyle='None')
+axs[0].plot(timegrid, timegrid * 0 + s.wstar[-1] * s.max_torque, 'r--')
 axs[0].set_xlabel('Time steps')
 axs[0].set_ylabel('Values')
 axs[0].legend()
 axs[0].grid(True)
 
 axs[1].plot(timegrid, states[:, 0], label='Position')
-axs[1].plot(timegrid, timegrid * 0 + s.xstar[0], 'r--')
 axs[1].plot(timegrid, states[:, 0] * events[:, 2], marker='o', markerfacecolor='none', linestyle='None')
+axs[1].plot(timegrid, timegrid * 0 + s.xstar[0], 'r--')
 axs[1].set_xlabel('Time steps')
 axs[1].set_ylabel('Values')
 axs[1].legend()

systems_and_LMI/systems/NonLinPendulum_kETM_train.py
@@ -50,18 +50,8 @@ class NonLinPendulum_kETM_train(NonLinPendulum_train):
       T = self.T[l]
       vec2 = (self.G[l] @ (self.state - self.xstar) - (self.last_w[l] - self.wstar[l]))
 
-      test_dz = False
-      if test_dz:
-        if np.linalg.norm(vec1 @ T @ vec2) < 1e-6:
-          lht = 0.0
-        else:
-          lht = (vec1 @ T @ vec2)[0][0]
-        rht = self.rho[l] * self.eta[l]
-        if rht < 1e-6:
-          rht = 0.0
-      else:
-        lht = (vec1 @ T @ vec2)[0][0]
-        rht = self.rho[l] * self.eta[l]
+      lht = (vec1 @ T @ vec2)[0][0]
+      rht = self.rho[l] * self.eta[l]
 
       check = lht > rht
       
@@ -71,22 +61,10 @@ class NonLinPendulum_kETM_train(NonLinPendulum_train):
         e[l] = 1
         vec1 = (nu - omega).T
         vec2 = (self.G[l] @ (self.state - self.xstar) - (omega - self.wstar[l]))
-        if test_dz:
-          if np.linalg.norm(vec1 @ T @ vec2) < 1e-6:
-            val[l] = 0.0
-          else:
-            val[l] = (vec1 @ T @ vec2)[0][0]
-        else:
-          val[l] = (vec1 @ T @ vec2)[0][0]
+        val[l] = (vec1 @ T @ vec2)[0][0]
       
       else:
-        if test_dz:
-          if np.linalg.norm(vec1 @ T @ vec2) < 1e-6:
-            val[l] = 0.0
-          else:
-            val[l] = (vec1 @ T @ vec2)[0][0]
-        else:
-          val[l] = (vec1 @ T @ vec2)[0][0]
+        val[l] = (vec1 @ T @ vec2)[0][0]
         omega = self.last_w[l]
         
     l = self.nlayer - 1

systems_and_LMI/systems/NonLinPendulum_train.py
@@ -62,12 +62,13 @@ class NonLinPendulum_train(NonLinPendulum):
     self.Rb = Rb
     
     def implicit_function(x):
-      x0 = x[0]
+      x = x.reshape(3, 1)
       I = np.eye(self.A.shape[0])
-      rhs = np.squeeze(np.linalg.inv(I - self.A - self.B @ self.Rw) @ (self.B @ self.Rb + self.C * (np.sin(x0) - x0) + self.D * self.constant_reference))
-      return x - rhs
-     
-    self.xstar = fsolve(implicit_function, np.array([[0.0], [0.0], [0.0]])).reshape(3,1)
+      K = np.array([[1.0, 0.0, 0.0]])
+      to_zero = np.squeeze((-I + self.A + self.B @ self.Rw - self.C @ K) @ x + self.C * np.sin(K @ x) + self.D * self.constant_reference + self.B @ self.Rb)
+      return to_zero
+
+    self.xstar = fsolve(implicit_function, np.array([[self.constant_reference], [0.0], [0.0]])).reshape(3,1)
 
     wstar = R @ Nvx @ self.xstar + R @ Nvb
     wstar1 = wstar[:self.neurons[0]]